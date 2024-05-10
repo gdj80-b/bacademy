f6d4cdd586c30d266e8ea4de7d8beefadcb38199
JoInHwan <dlsghks135@naver.com>
2024-05-10 20:50:02 +0900

    조인환 : <Feature> 위시리스트 출력기능 구현 - WishList Controller 작성 - WishList Mapper 작성 (wishlist테이블,lecture테이블 조인) - WishList VO 작성 - WishList View 작성

--- src/main/java/com/goodee/bacademy/vo/Wishlist.java
+++ src/main/java/com/goodee/bacademy/vo/Wishlist.java
@@ -1,5 +1,14 @@
 package com.goodee.bacademy.vo;
 
-public class Wishlist {
+import lombok.Data;
 
+@Data
+public class WishList {
+	private String lecture_name;
+	private String lecture_price;
+	private String room_num;
+	private String lecture_time;
+	private String lecture_day;
+	private String start_date;
+	private String end_date;
 }
