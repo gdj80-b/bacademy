98dea5b27f81fdcd5335dd4f4ea656434ff1b083
SungHoCha <tjdgh3725@gmail.com>
2024-05-13 09:55:30 +0900

    차성호 <Feature> 로그인 관련 기능 추가
    -MemberJoinController
    -LoginController

--- src/main/java/com/goodee/bacademy/mapper/LoginMapper.java
+++ src/main/java/com/goodee/bacademy/mapper/LoginMapper.java
@@ -1,5 +1,12 @@
 package com.goodee.bacademy.mapper;
 
-public interface LoginMapper {
+import java.util.Map;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.goodee.bacademy.vo.Member;
+
+@Mapper
+public interface LoginMapper {
+	Map<String, Object> validateMember(Member member);
 }
