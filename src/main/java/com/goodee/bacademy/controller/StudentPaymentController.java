6e84495ee37e608f2a33a890808a97e683dd350c
JoInHwan <dlsghks135@naver.com>
2024-05-13 12:00:38 +0900

    조인환 : <Feature> 내캐쉬내역 출력기능 구현 , <Fix> myWish - StudentPaument Controller 작성 - StudentPayment Mapper 작성 (application테이블, cashHistory테이블 & lecture테이블 조인) - PaymentVO 생성 후 작성 - myCashList View 생성 작성
    - WishListVO - snake표현식에서 카멜표현식으로 수정
    - WishListMapper - AS로 snake 표현식에서 카멜표현식으로 수정
    - WishLisy View - snake 표현식에서 카멜표현식으로 수정

--- src/main/java/com/goodee/bacademy/controller/StudentPaymentController.java
+++ src/main/java/com/goodee/bacademy/controller/StudentPaymentController.java
@@ -1,5 +1,27 @@
 package com.goodee.bacademy.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.goodee.bacademy.mapper.StudentPaymentMapper;
+import com.goodee.bacademy.vo.PaymentVO;
+
+@Controller
 public class StudentPaymentController {
 
+	
+	@Autowired
+	private StudentPaymentMapper mapper;
+	
+	@GetMapping("/myCashList")
+	public String myCashList(Model model) {
+		List<PaymentVO> list = mapper.getMyCashList();
+		model.addAttribute("myCashList", list);
+		return "myCashList";
+	}
+	
 }
--- src/main/java/com/goodee/bacademy/vo/WishListVO.java
+++ src/main/java/com/goodee/bacademy/vo/WishListVO.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 @Data
 public class WishListVO {
-	private String lecture_no;		// JOIN으로 강의 테이블에서 가져온 컬럼
-	private String lecture_name;	// JOIN으로 강의 테이블에서 가져온 컬럼
-	private String lecture_price;	// JOIN으로 강의 테이블에서 가져온 컬럼
-	private String room_num;		// JOIN으로 강의 테이블에서 가져온 컬럼
-	private String lecture_time;	// JOIN으로 강의 테이블에서 가져온 컬럼
-	private String lecture_day;		// JOIN으로 강의 테이블에서 가져온 컬럼
-	private String start_date;		// JOIN으로 강의 테이블에서 가져온 컬럼
-	private String end_date;		// JOIN으로 강의 테이블에서 가져온 컬럼
+	private String lectureNo;		// JOIN으로 강의 테이블에서 가져온 컬럼
+	private String lectureName;		// JOIN으로 강의 테이블에서 가져온 컬럼
+	private String lecturePrice;	// JOIN으로 강의 테이블에서 가져온 컬럼
+	private String roomNum;			// JOIN으로 강의 테이블에서 가져온 컬럼
+	private String lectureTime;		// JOIN으로 강의 테이블에서 가져온 컬럼
+	private String lectureDay;		// JOIN으로 강의 테이블에서 가져온 컬럼
+	private String startDate;		// JOIN으로 강의 테이블에서 가져온 컬럼
+	private String endDate;			// JOIN으로 강의 테이블에서 가져온 컬럼
 }
