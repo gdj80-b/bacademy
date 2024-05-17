2d97253b853132826d832b8d173753350a715723
geonhee <geonhee.boot@gmail.com>
2024-05-17 10:22:11 +0900

    정건희 : <Fix> 직원 조회에서 상세보기 수정 - 강사만 상세보기에서 + 직원 상세보기 추가 - 직원 전체 조회에서 페이지네이션에 for문으로 페이지 수에 맞게 숫자 버튼 생성

--- src/main/java/com/goodee/bacademy/mapper/MemberInfoMapper.java
+++ src/main/java/com/goodee/bacademy/mapper/MemberInfoMapper.java
@@ -11,8 +11,14 @@ import com.goodee.bacademy.vo.PagingVO;
 
 @Mapper
 public interface MemberInfoMapper {
+	
+	// 차성호 : 나의 강의 목록 조회
 	List<LectureVO> getMyLectureList(String id);
+	
+	// 차성호 : 현재 강의 목록 조회
 	List<LectureVO> getCurrentLectureList(Map<String, String> lectureInfo);
+	
+	// 차성호 : 직원 상세 조회
 	Map<String, Object> getEmpOne(String id);
 	
 	// 정건희 : 직원 전체 조회
