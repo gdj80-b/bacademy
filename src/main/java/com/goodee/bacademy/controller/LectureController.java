0b3f1f48afb520b9615adbba6db9dcfc06d55640
kimukih <hyeongh94@gmail.com>
2024-05-28 20:34:17 +0900

    김형호: <Fix> lectureList 헤더,사이드바 추가 - LectureController 주석 추가 - 불필요한 코드 및 주석 삭제

--- src/main/java/com/goodee/bacademy/controller/LectureController.java
+++ src/main/java/com/goodee/bacademy/controller/LectureController.java
@@ -25,30 +25,33 @@ public class LectureController {
 	private LectureMapper lectureMapper;
 	
 	// addLectureForm.jsp
+	// 강의 등록 페이지 이동
 	@GetMapping("/addLectureForm")
 	public String addLectureForm() {
 		return "lecture/addLectureForm";
 	}
 	
 	// addLectureAction
+	// 강의 등록 액션
 	@PostMapping("/lecture/addLecture")
 	public String addLecture(LectureVO lecture) {
 		int addLectureRow = lectureMapper.addLecture(lecture);
 		
 		if(addLectureRow == 1) {
 			System.out.println("강의 등록 성공");
-			return "redirect:/lectureList";
+			return "redirect:/lectureList";		// 강의 등록 성공한 경우 강의목록으로 이동
 		}else {
 			System.out.println("강의 등록 실패");
-			return "redirect:/addLectureForm";
+			return "redirect:/addLectureForm";	// 강의 등록 실패한 경우 강의추가 페이지로 재요청
 		}
 	}
 	
 	// lectureList.jsp
+	// 강의 목록 페이지
 	@GetMapping("/lectureList")
 	public String lectureList(Model model, @ModelAttribute("paging")PagingVO paging) {
 		
-		int totalRow = lectureMapper.getTotalRow(paging);
+		int totalRow = lectureMapper.getTotalRow(paging);	// 페이징을 위한 전체 강의 목록 수
 		
 		paging.setTotalRow(totalRow);
 		paging.pageSetting();
@@ -59,6 +62,7 @@ public class LectureController {
 	}
 	
 	// lectureOne.jsp
+	// 강의 상세 페이지
 	@GetMapping("/lectureOne")
 	public String lectureOne(Model model, @RequestParam(value="lectureNo") int lectureNo) {
 		List<LectureVO> lectureOneList = lectureMapper.getLectureOne(lectureNo);
@@ -67,6 +71,7 @@ public class LectureController {
 	}
 	
 	// modifyLectureForm.jsp
+	// 강의 수정 페이지 이동
 	@GetMapping("/modifyLectureForm")
 	public String modifyLectureForm(Model model, @RequestParam(value="lectureNo") int lectureNo) {
 		List<LectureVO> lectureOneList = lectureMapper.getLectureOne(lectureNo);
@@ -75,6 +80,7 @@ public class LectureController {
 	}
 	
 	// modifyLectureAction
+	// 강의 수정 액션
 	@PostMapping("/lecture/modifyLecture")
 	public String modifyLecture(LectureVO lecture) {
 		
@@ -84,14 +90,15 @@ public class LectureController {
 		
 		if(modifyLectureRow == 1) {
 			System.out.println("강의 수정 성공");
-			return "redirect:/lectureOne?lectureNo=" + lectureNo;
+			return "redirect:/lectureOne?lectureNo=" + lectureNo;			// 강의 수정 성공 시 해당 강의상세 재요청
 		}else {
 			System.out.println("강의 수정 실패");
-			return "redirect:/modifyLectureForm?lectureNo=" + lectureNo;
+			return "redirect:/modifyLectureForm?lectureNo=" + lectureNo;	// 강의 수정 실패 시 강의 수정 폼으로 재요청
 		}
 	}
 	
 	// deleteLectureAction
+	// 강의 삭제 액션
 	@GetMapping("/deleteLecture")
 	public String deleteLecture(LectureVO lecture) {
 		
@@ -101,54 +108,37 @@ public class LectureController {
 		
 		if(deleteLectureRow == 1) {
 			System.out.println("강의 삭제 성공");
-			return "redirect:/lectureList";
+			return "redirect:/lectureList";							// 강의 삭제 성공 시 강의목록 재요청
 		}else {
 			System.out.println("강의 삭제 실패");
-			return "redirect:/lectureOne?lectureNo=" + lectureNo;
+			return "redirect:/lectureOne?lectureNo=" + lectureNo;	// 강의 삭제 실패 시 해당 강의상세 재요청
 		}
 	}
 	
-	/*
 	// addLectureConfirm
+	// 등록 가능한 강의 유효성 검사
 	@ResponseBody // ajax에 data 전달하는 용도로 String 반환
 	@GetMapping("/lectureConfirm")
 	public int lectureConfirm(
-			@RequestParam("lectureDay") String lectureDay, @RequestParam("roomNum") int roomNum,
-			@RequestParam("id") String id
-			) {
+			@RequestParam(value="startDate") String startDate, 
+			@RequestParam(value="endDate") String endDate, 
+			@RequestParam(value="lectureDay") String lectureDay, 
+			@RequestParam(value="roomNum") String roomNum, 
+			@RequestParam(value="id") String id) {
+		
+		System.out.println("startDate : " + startDate);
+		System.out.println("endDate : " + endDate);
+		System.out.println("id : " + id);
 		System.out.println("lectureDay : " + lectureDay);
 		System.out.println("roomNum : " + roomNum);
-		System.out.println("id : " + id);
 		
-		LectureVO lecture = lectureMapper.lectureConfirm(lectureDay, roomNum, id);
-		if(lecture != null) {
+		// 강의시작일, 강의종료일, 강의요일, 강의실, 강사 유효성 체크
+		List<LectureVO> lecture = lectureMapper.lectureConfirm(startDate, endDate, lectureDay, roomNum, id);
+
+		if(lecture.size() > 0) {
 			return 0;	// 이미 존재하는 강의 정보, 등록 불가
 		}
 		return 1;	// 등록 가능한 강의
 	}
-	*/
-	
-	// addLectureConfirm
-		@ResponseBody // ajax에 data 전달하는 용도로 String 반환
-		@GetMapping("/lectureConfirm")
-		public int lectureConfirm(
-				@RequestParam(value="startDate") String startDate,
-				@RequestParam(value="endDate") String endDate,
-				@RequestParam(value="lectureDay") String lectureDay, 
-				@RequestParam(value="roomNum") String roomNum, 
-				@RequestParam(value="id") String id) {
-			
-			System.out.println("startDate : " + startDate);
-			System.out.println("endDate : " + endDate);
-			System.out.println("id : " + id);
-			System.out.println("lectureDay : " + lectureDay);
-			System.out.println("roomNum : " + roomNum);
-			
-			List<LectureVO> lecture = lectureMapper.lectureConfirm(startDate, endDate, lectureDay, roomNum, id);
-			if(lecture.size() > 0) {
-				return 0;	// 이미 존재하는 강의 정보, 등록 불가
-			}
-			return 1;	// 등록 가능한 강의
-		}
 	
 }
