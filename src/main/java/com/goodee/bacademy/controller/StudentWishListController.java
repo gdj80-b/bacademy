6c325ec39fb53eb71b19750d3aa9a550930a7b4e
JoInHwan <dlsghks135@naver.com>
2024-05-29 11:38:28 +0900

    조인환: <Feature> 캐쉬충전에따른 세션의 cash값 변경기능 구현

--- src/main/java/com/goodee/bacademy/controller/StudentWishListController.java
+++ src/main/java/com/goodee/bacademy/controller/StudentWishListController.java
@@ -11,9 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.goodee.bacademy.mapper.CashMapper;
 import com.goodee.bacademy.mapper.StudentWishListMapper;
+import com.goodee.bacademy.mapper.StudentPaymentMapper;
 import com.goodee.bacademy.vo.WishListVO;
 
 import jakarta.servlet.http.HttpSession;
@@ -26,8 +28,14 @@ public class StudentWishListController {
 	private StudentWishListMapper mapper;
 	// 내 찜 리스트 조회
 	@GetMapping("/myWishList")
-	public String myWishList(HttpSession session, Model model) {
-		Map<String, Object> loginInfo =(Map<String, Object>) session.getAttribute("loginInfo");	
+	public String myWishList(HttpSession session, Model model, RedirectAttributes rattr) {
+		Map<String, Object> loginInfo =(Map<String, Object>) session.getAttribute("loginInfo");
+		// 로그인 성공 여부에 따른 분기
+		if (loginInfo == null || loginInfo.isEmpty()) {
+			rattr.addFlashAttribute("msgType", "로그인 실패 메시지");
+			rattr.addFlashAttribute("msg", "아이디 또는 비밀번호를 확인해주세요.");
+			return "redirect:/loginForm"; // 로그인 실패 시 로그인 페이지로 이동
+		}
 		String id = (String)(loginInfo.get("id"));
 		List<WishListVO> list = mapper.getMyWishList(id);
 		int myCash = (int)(loginInfo.get("cash"));
@@ -63,7 +71,7 @@ public class StudentWishListController {
 		
 	}
 	
-	// 내찜 삭재
+	// 내찜 삭제
 		@Autowired
 		private StudentWishListMapper deleteWishMapper;
 		
@@ -100,11 +108,14 @@ public class StudentWishListController {
 		private StudentWishListMapper addApplicationMapper;
 		@Autowired
 		private CashMapper cashMapper;
+		@Autowired
+		private StudentPaymentMapper studentPaymentMapper;
 		@PostMapping("/addApplication")
 		public String addApplication(
 					@RequestParam(name="id") String id,
 					@RequestParam(name="lectureNo", required=false) String[] lectureNo,
-					@RequestParam(name="cash") String cash
+					@RequestParam(name="cash") String cash,
+					HttpSession session
 				)
 		{	
 			Map<String,Object>wishMap = new HashMap<>();
@@ -123,12 +134,16 @@ public class StudentWishListController {
 		            int addApplicationResult = addApplicationMapper.addApplication(wishMap); // 수강신청 리스트에 추가
 		            
 		        }
-		        Map<String,Object>refundMap = new HashMap<>(); // 학생 캐쉬업데이트(차감)
-		        
-		        
+		        Map<String,Object>refundMap = new HashMap<>(); // 학생 캐쉬업데이트(차감)		        
 		        refundMap.put("id", id);
 		        refundMap.put("cash", cash);
-		        int updateStudentResult = cashMapper.updateStudentCash(refundMap);				
+		        int updateStudentResult = cashMapper.updateStudentCash(refundMap);	
+		        // 바뀐 금액 세션값에 다시저장
+//		        String myCash = studentPaymentMapper.getMyCash(id);				
+//				Map<String, String> loginInfo =(Map<String, String>) session.getAttribute("loginInfo");
+//				loginInfo.put("cash", myCash);
+//				session.setAttribute("loginInfo", loginInfo);
+				
 		        System.out.println("수강신청성공");
 		        return "redirect:/myWishList";
 		        
